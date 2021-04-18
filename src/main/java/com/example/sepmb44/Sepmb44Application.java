23c4ab355779d14ff45cedd4875e37035d2687ef
SengLongHuo <s3781162@student.rmit.edu.au>
2021-04-18 20:34:23 +1000

    Initial Commit - Basic structure

---
+++ src/main/java/com/example/sepmb44/Sepmb44Application.java
@@ -0,0 +1,13 @@
+package com.example.sepmb44;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sepmb44Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sepmb44Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sepmb44/Sepmb44ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sepmb44;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sepmb44ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
